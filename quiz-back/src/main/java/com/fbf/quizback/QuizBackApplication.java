28121a2e72f5d269ae2aadba902d92de051c06af
Fabian <Fabian@192.168.0.110>
2018-05-25 19:27:50 +0200

    Iniciando...

---
+++ quiz-back/src/main/java/com/fbf/quizback/QuizBackApplication.java
@@ -0,0 +1,14 @@
+package com.fbf.quizback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBackApplication.class, args);
+	}
+}
+
+
